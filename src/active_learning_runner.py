0e6f70359298496e7cfda5c8a6e6bc2ccee5588e
Ubuntu <ubuntu@activecellacdc-83f0d.openstack.bielefeld>
2022-11-18 22:10:33 +0000

    change al config

--- src/active_learning_runner.py
+++ src/active_learning_runner.py
@@ -8,6 +8,6 @@ from active_learning.active_learning_trainer import ActiveLearningTrainer
 if __name__ == "__main__":
     running_on_server = os.getenv("IS_SERVER") == "true"
     print("running on server:", running_on_server)
-    cfg = get_config("al_pipeline_config_1_2")
+    cfg = get_config("cellpose_al_config_50_50")
     al_trainer = ActiveLearningTrainer(cfg, is_test_mode=not running_on_server)
-    al_trainer.run(dataset=CELLPOSE, query_strat=RANDOM)
+    al_trainer.run(dataset=CELLPOSE, query_strat=KNOWN_VALIDATION)
