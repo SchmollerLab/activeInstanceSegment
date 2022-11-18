3a597c58652daa8634ec57383a59331760e61f07
Ubuntu <ubuntu@activecellacdc-83f0d.openstack.bielefeld>
2022-11-18 22:17:05 +0000

    replace unzip with 7z

--- shell_scripts/downloadDataLarge.sh
+++ shell_scripts/downloadDataLarge.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir data/raw_data
 mkdir data/raw_data/acdc_large
 
 cd data/raw_data/acdc_large
@@ -8,14 +7,14 @@ cd data/raw_data/acdc_large
 wget https://hmgubox2.helmholtz-muenchen.de/index.php/s/x3eg2iiaLJnbzCY/download/acdc_data2.zip
 wget https://hmgubox2.helmholtz-muenchen.de/index.php/s/drXrEjZpYQN4KZT/download/acdc_data3.zip
 
-if ! command -v unzip /dev/null
+if ! command -v 7z /dev/null
 then
-    sudo apt-get install unzip -y
+    sudo apt install p7zip-full -y
 fi
-unzip acdc_data2.zip -d acdc_data2
+7z x acdc_data2.zip
 rm acdc_data2.zip
 
-unzip acdc_data3.zip -d acdc_data3
+7z x acdc_data3.zip
 rm acdc_data3.zip
 
 cd ../../..
