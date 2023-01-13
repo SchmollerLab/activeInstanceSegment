b0edafe30831b8937ddb1d3c119aaf9c107dfbd5
Florian Bridges <florianbridges97@gmail.com>
2023-01-13 13:58:13 +0100

    update install.sh

--- shell_scripts/install.sh
+++ shell_scripts/install.sh
@@ -13,7 +13,7 @@ echo installing cuda
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2204-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update -y
 sudo apt-get -y install cuda
@@ -36,6 +36,7 @@ project_root="$(pwd)"/activeCell-ACDC
 data_path="$(pwd)"/activeCell-ACDC/data
 mkdir $data_path
 mkdir $data_path/raw_data
+mkdir $project_root/output
 
 if [[ -z $IS_SERVER ]]; then
     echo "enviromental IS_SERVER is not set. appending to .bashrc"
@@ -58,6 +59,6 @@ else
     echo "enviromental DATA_PATH is already set."
 fi
 
-echo setting up data
+echo setting up datapip
 $project_root/shell_scripts/downloadDataLarge.sh
 python src/dataloader/data2coco.py
