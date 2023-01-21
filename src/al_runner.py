1e061a6ce495675cb969b228be590c536242c8b0
Florian Bridges <florianbridges97@gmail.com>
2023-01-21 08:13:55 +0100

    change seed for different al runs

--- src/al_runner.py
+++ src/al_runner.py
@@ -8,8 +8,9 @@ from active_learning.al_trainer import ActiveLearningTrainer
 if __name__ == "__main__":
     running_on_server = os.getenv("IS_SERVER") == "true"
     print("running on server:", running_on_server)
-    cfg = get_config("acdc_large_al")
+    cfg = get_config("debug_acdc_large_al")
     for _ in range(5):
+        cfg.SEED += 1
         al_trainer = ActiveLearningTrainer(cfg, is_test_mode=not running_on_server)
         al_trainer.run(dataset=ACDC_LARGE, query_strat=MC_DROPOUT)
         al_trainer.run(dataset=ACDC_LARGE, query_strat=RANDOM)
