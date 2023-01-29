883fab0666a0916f94e3440827cf6c4001aa373a
Florian Bridges <florianbridges97@gmail.com>
2023-01-29 15:53:52 +0100

    change al runner

--- src/al_runner.py
+++ src/al_runner.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     cur_date = str(date.today().month) + str(date.today().day)
     for _ in range(5):
         cfg.SEED += 1
-        for mode in ["sum_50", "quant_20","sum"]:
-            cfg.AL.OBJECT_TO_IMG_AGG = mode
-            al_trainer = ActiveLearningTrainer(cfg, is_test_mode=not running_on_server)
-            al_trainer.run(dataset=ACDC_LARGE, query_strat=MC_DROPOUT, cur_date=cur_date + mode)
+        al_trainer = ActiveLearningTrainer(cfg, is_test_mode=not running_on_server)
+        al_trainer.run(dataset=ACDC_LARGE, query_strat=MC_DROPOUT, cur_date=cur_date)
+        al_trainer.run(dataset=ACDC_LARGE, query_strat=RANDOM, cur_date=cur_date)
