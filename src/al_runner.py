adb050ed4eaf0a5d9accb80feaf8328f1d64a53e
Florian Bridges <florianbridges97@gmail.com>
2023-02-04 07:56:10 +0100

    add acdc ds without classes

--- src/al_runner.py
+++ src/al_runner.py
@@ -8,7 +8,7 @@ from active_learning.al_trainer import ActiveLearningTrainer
 if __name__ == "__main__":
     running_on_server = os.getenv("IS_SERVER") == "true"
     print("running on server:", running_on_server)
-    cfg = get_config("classes_acdc_large_al")
+    cfg = get_config("acdc_large_al")
 
     cur_date = str(date.today().month) + str(date.today().day)
     for _ in range(5):
