806b8d1ee060b6f31f24cd454ae6af82ddaf7776
Florian Bridges <florianbridges97@gmail.com>
2023-03-06 11:53:48 +0100

    change to random

--- src/al_runner.py
+++ src/al_runner.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     config_name = config_filename.split("/")[-1].replace(".yaml", "")
     cfg = get_config(config_name, complete_path=config_filename)
 
-    cur_date = "long_run_kmeans" + str(date.today().month) + str(date.today().day)
+    cur_date = "" + str(date.today().month) + str(date.today().day)
     for _ in range(2):
         cfg.SEED += 1
         al_trainer = ActiveLearningTrainer(
